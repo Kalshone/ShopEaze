41e79ac8874f86cad941a429764ad0bcec77bb32
Andrey Zinovyev <andrey.znvyev@gmail.com>
2023-07-26 19:50:55 -0400

    Added a welcome screen, edited that navigation file to include these changes

--- app/src/main/java/com/example/shopeaze/WelcomeScreen.java
+++ app/src/main/java/com/example/shopeaze/WelcomeScreen.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -29,14 +30,25 @@ public class WelcomeScreen extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        Button shopperButton = view.findViewById(R.id.button_shopper);
+        shopperButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(WelcomeScreen.this)
-                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                        .navigate(R.id.action_FirstFragment_to_ShopperFragment);
+            }
+        });
+
+        Button ownerButton = view.findViewById(R.id.button_owner);
+        ownerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(WelcomeScreen.this)
+                        .navigate(R.id.action_FirstFragment_to_OwnerFragment);
             }
         });
     }
+
 
     @Override
     public void onDestroyView() {
