be37cf045116ef96398644b3c979820fbfb0a04a
chr1syanner <86560918+chrisyan04@users.noreply.github.com>
2023-07-27 13:02:23 -0400

    Updating Product class and Creating ProductList class. (#7)

--- app/src/main/java/com/example/shopeaze/Product.java
+++ app/src/main/java/com/example/shopeaze/Product.java
@@ -1,29 +1,45 @@
 package com.example.shopeaze;
 
+import java.util.UUID;
 public class Product {
+    // Fields
+    private String productID;
     private String productName;
     private String productBrand;
-    double productPrice;
+    private String productDescription;
+    private double productPrice;
 
-    public Product(){
-    }
-
-    public Product(String name, String brand, double price) {
+    // Constructor
+    public Product(String name, String brand, double price, String description) {
+        this.productID = generateProductID();
         this.productName = name;
         this.productBrand = brand;
         this.productPrice = price;
+        this.productDescription = description;
     }
 
+    private String generateProductID() {
+        String rand = UUID.randomUUID().toString();
+        return rand;
+    }
+
+    // Getters
+    public String getProductID() { return productID; }
     public String getName() {
         return productName;
     }
-
     public String getBrand() {
         return productBrand;
     }
-
+    public String getDescription() { return productDescription; }
     public double getPrice() {
         return productPrice;
     }
 
+    // Setters
+    public void setProductID(String id) { this.productID = id; }
+    public void setName(String name) { this.productName = name; }
+    public void setPrice(double price) { this.productPrice = price; }
+    public void setDescription(String description) { this.productDescription = description; }
+    public void setBrand(String brand) { this.productBrand = brand; }
 }
