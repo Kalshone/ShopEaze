aa4aa37811e22446ab133661e6bae1c7db3e171e
Andrey Zinovyev <andrey.znvyev@gmail.com>
2023-08-03 16:08:13 -0400

    Modified Order.java to include the userId

--- app/src/main/java/com/example/shopeaze/Order.java
+++ app/src/main/java/com/example/shopeaze/Order.java
@@ -2,21 +2,40 @@ package com.example.shopeaze;
 
 public class Order {
     private String orderId;
+    private String userId;
     private String status;
 
-    public Order() {
-    }
+    // Empty constructor needed for Firestore
+    public Order() {}
 
-    public Order(String orderId, String status) {
-        this.orderId = orderId;
+    public Order(String userId, String status) {
+        this.userId = userId;
         this.status = status;
     }
 
+    // Getter methods
     public String getOrderId() {
         return orderId;
     }
 
+    public String getUserId() {
+        return userId;
+    }
+
     public String getStatus() {
         return status;
+    }
+
+    // Setter methods
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
